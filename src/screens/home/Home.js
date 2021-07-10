08319479ad93caad6f264f895a8cb21586c13259
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2021-07-10 13:50:20 +0530

    Added 'Release Date End' filter for movies on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -157,11 +157,21 @@ class Home extends Component {
                                         ))}
                                     </Select>
                                 </FormControl>
-                                
+
                                 <FormControl className={classes.formControl}>
                                     <TextField
                                         id="releaseDateStart"
                                         label="Release Date Start"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{ shrink: true }}
+                                    />
+                                </FormControl>
+                                
+                                <FormControl className={classes.formControl}>
+                                    <TextField
+                                        id="releaseDateEnd"
+                                        label="Release Date End"
                                         type="date"
                                         defaultValue=""
                                         InputLabelProps={{ shrink: true }}
